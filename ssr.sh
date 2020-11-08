2da032e9874eb22b5f5230d7e9f5371c576ff9c6
KomoreKalu <komorekalu@outlook.com>
2020-11-09 00:02:55 +0800

    Add ssr.sh

---
+++ ssr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ssr(){
+
+        stop(){
+                uci set shadowsocksr.cfg013fd6.global_server=nil
+                uci commit shadowsocksr
+                /etc/init.d/shadowsocksr stop
+        }
+
+        start(){
+                uci set shadowsocksr.cfg013fd6.global_server=cfg094a8f
+                uci commit shadowsocksr
+                /etc/init.d/shadowsocksr start
+        }
+
+}
+ssr
+$1
+
