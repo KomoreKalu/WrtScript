f1076833eb0674daf593e6a6319765cbdcabbeb4
KomoreKalu <komorekalu@outlook.com>
2020-11-23 23:52:32 +0800

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -2,11 +2,10 @@
 . ./hostCheck.sh
 . ./ssr.sh
 . ./dhcp.sh
-HOST=172.16.7.1
-if [ "hostCheck $HOST" -gt 0 ] ; then	
+sleep 120 
+HOST='172.16.7.1'
+if [ $(hostCheck $HOST) -gt 0 ] ; then	
 	ssr stop
 	dhcp setup
-else
-	ssr start
-	dhcp fallback
+	wifi
 fi
