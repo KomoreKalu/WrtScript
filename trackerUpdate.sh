388c8324d44d83c9b8cef08892f5aba6a87d12f2
KomoreKalu <komorekalu@outlook.com>
2020-12-13 02:25:36 +0800

    Add bt_tracker_enable

--- trackerUpdate.sh
+++ trackerUpdate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 URL="https://cdn.jsdelivr.net/gh/ngosang/trackerslist/trackers_best.txt"
 
+uci set aria2.main.bt_tracker_enable=1
+
 curl -s $URL | tr -s '\n' | while read line 
 do
 	uci add_list aria2.main.bt_tracker="$line"
